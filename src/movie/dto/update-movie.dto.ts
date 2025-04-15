b257541d1c6a123d19dd59578120e68a6d27937b
kimminjae981002 <145568228+kimminjae981002@users.noreply.github.com>
2025-04-15 21:30:43 +0900

    [FEAT] Basic class-validator 1. @IsDefined null || undefined 가 나오면 에러 2. @Equals('string') string과 같아야한다. 3. @NotEquals('string') string과 달라야한다. 4. @IsEmpty() null || undefined || '' 가 안 나와야한다. 5. @IsIn(['test','test2']) 배열 안에 있는 값 중 하나이어야한다. 6. @IsNotIn(['test','test3']) 배열 안에 있는 값 중 아니어야한다.

--- src/movie/dto/update-movie.dto.ts
+++ src/movie/dto/update-movie.dto.ts
@@ -1,4 +1,13 @@
-import { IsNotEmpty, IsOptional } from 'class-validator';
+import {
+  Equals,
+  IsDefined,
+  IsEmpty,
+  IsIn,
+  IsNotEmpty,
+  IsNotIn,
+  IsOptional,
+  NotEquals,
+} from 'class-validator';
 
 export class UpdateMovieDto {
   @IsNotEmpty()
@@ -6,4 +15,28 @@ export class UpdateMovieDto {
 
   @IsOptional()
   genre?: string;
+
+  // null || undefined 이면 에러를 던진다.
+  @IsDefined()
+  test1: string;
+
+  // hello와 같지 않으면 에러를 던진다.
+  @Equals('hello')
+  test2: string;
+
+  // world와 같으면 에러를 던진다.
+  @NotEquals('world')
+  test3: string;
+
+  // null || undefined || '' 가 안 나오면 에러를 던진다.
+  @IsEmpty()
+  test4: string;
+
+  // 배열 안에 있는 값 중 하나이어야한다.
+  @IsIn(['test5', 'test'])
+  test5: string;
+
+  // 배열 안에 있는 값이 아니어야한다..
+  @IsNotIn(['test15', 'test16'])
+  test6: string;
 }
