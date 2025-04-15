31ef768499436d73343a0719719fdc789a8ef1c0
kimminjae981002 <145568228+kimminjae981002@users.noreply.github.com>
2025-04-15 21:38:44 +0900

    [FEAT] Type class-validator 1. @IsNumber 숫자만 2. @IsString 문자만 3. @IsBoolean 불리언만 4. @IsInt 정수만 5. @IsArray 배열만 6. @IsEnum Enum에 있는 것만 7. @IsDate 날짜 객체만 8. @IsDateString 날짜 객체 문자열만

--- src/movie/dto/update-movie.dto.ts
+++ src/movie/dto/update-movie.dto.ts
@@ -1,18 +1,35 @@
 import {
   Equals,
+  IsArray,
+  IsBoolean,
+  IsDate,
+  IsDateString,
   IsDefined,
   IsEmpty,
+  IsEnum,
   IsIn,
+  IsInt,
   IsNotEmpty,
   IsNotIn,
+  IsNumber,
   IsOptional,
+  IsString,
   NotEquals,
 } from 'class-validator';
 
+enum movieGenre {
+  action = 'action',
+  fantasy = 'fantasy',
+}
+
 export class UpdateMovieDto {
+  // basic
+
+  // 없으면 안된다.
   @IsNotEmpty()
   title?: string;
 
+  // 있어도 되고 없어도 된다.
   @IsOptional()
   genre?: string;
 
@@ -39,4 +56,38 @@ export class UpdateMovieDto {
   // 배열 안에 있는 값이 아니어야한다..
   @IsNotIn(['test15', 'test16'])
   test6: string;
+
+  // type
+
+  // 숫자만
+  @IsNumber()
+  test7: number;
+
+  // 문자만
+  @IsString()
+  test8: string;
+
+  // 불리언만
+  @IsBoolean()
+  test9: boolean;
+
+  // 정수만
+  @IsInt()
+  test10: number;
+
+  // 배열만
+  @IsArray()
+  test11: string[];
+
+  // Enum 타입만
+  @IsEnum(movieGenre)
+  test12: string;
+
+  // 실제 날짜 객체
+  @IsDate()
+  test13: Date;
+
+  // 날짜 스트링
+  @IsDateString()
+  test14: string;
 }
