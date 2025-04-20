b8600aa0950bef25b8cbe61897e38ebcfe784979
kimminjae981002 <145568228+kimminjae981002@users.noreply.github.com>
2025-04-20 18:23:36 +0900

    [UPDATE] MovieTitlePipe 1. Title이 작성 안 되면 return

--- src/movie/pipe/movie-title-validation.pipe.ts
+++ src/movie/pipe/movie-title-validation.pipe.ts
@@ -8,6 +8,9 @@ import {
 @Injectable()
 export class MovieTitleValidationPipe implements PipeTransform<string, string> {
   transform(value: string, metadata: ArgumentMetadata): string {
+    if (!value) {
+      return value;
+    }
     // Title이 길이가 2보다 작으면 에러
     if (value.length <= 2) {
       throw new BadRequestException('영화의 제목은 3글자 이상 작성해주세요.');
