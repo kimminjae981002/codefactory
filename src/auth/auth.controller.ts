c51507f2a308708a16dd780337afe3b924559843
kimminjae981002 <145568228+kimminjae981002@users.noreply.github.com>
2025-04-21 21:43:28 +0900

    [FEAT] Auth User SignUp 1. 유저 회원가입 시 토큰 검증 및 bcrypt를 이용한 해쉬 후 패스워드 저장

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { Controller, Post, Headers } from '@nestjs/common';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+  @Post()
+  async signUp(@Headers('authorization') token: string) {
+    return await this.authService.signUp(token);
+  }
+}
