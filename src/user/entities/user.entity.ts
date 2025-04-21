45b0a4b61dec17b79cb87a50fbdf73e2b273c837
kimminjae981002 <145568228+kimminjae981002@users.noreply.github.com>
2025-04-21 21:17:58 +0900

    [FEAT] 유저 엔티티 설정

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,23 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+export enum Role {
+  admin,
+  paidUser,
+  user,
+}
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    unique: true,
+  })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ enum: Role, default: Role.user })
+  role: Role;
+}
